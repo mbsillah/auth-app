32966dc91301afd0e0a0f6c299d1b43cb522ea03
sillah2010 <macktastic2014@gmail.com>
2019-03-31 21:20:50 -0400

    models and validation created

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected!!') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
 
 const app = express();
 
@@ -12,7 +19,7 @@ app.get('/', function(req, res) {
     res.send('hello');
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
