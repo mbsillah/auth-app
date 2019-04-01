3f202b36a47bb8745b17ae2fb5b34c7d8bf54e76
sillah2010 <macktastic2014@gmail.com>
2019-03-31 21:48:10 -0400

    Backend done

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const users = require('./routes/user'); 
+
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
     () => {console.log('Database is connected!!') },
@@ -11,9 +13,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
 );
 
 const app = express();
+app.use(passport.initialize());
+require('./passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api/users', users);
 
 app.get('/', function(req, res) {
     res.send('hello');
