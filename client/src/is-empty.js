f62ac7cc58da43ab8d515e9e1f85ca1a7d7d821f
sillah2010 <macktastic2014@gmail.com>
2019-04-11 18:29:06 -0400

    App completed

---
+++ client/src/is-empty.js
@@ -0,0 +1,11 @@
+const isEmpty = (value) => {
+    return (
+        value === undefined ||
+        value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0) ||
+        (typeof value === 'string' && value.trim().length === 0)
+    );
+}
+export default isEmpty;
+
+//We need this file when we check whether the user is authenticated or not.
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
     res.send('hello');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
